387017f1ef823e297e01cd5fb32045eb034fdaf1
jhenrique04 <jhenriquevs.04@gmail.com>
2023-12-26 01:12:23 -0300

    feat: implement weather maps

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-This Telegram bot allows users to search for weather information using the OpenWeatherMap API. Users can get current weather data, a 5-day forecast, and meteorological conditions for a specified location directly within Telegram.
+This Telegram bot allows users to search for weather information using the OpenWeatherMap API. Users can get current weather data, a 5-day forecast, meteorological conditions for a specified location and various types of maps (clouds, precipitation, pressure, wind and temperature) directly within Telegram.
 
 ## Features
 
@@ -11,6 +11,8 @@ This Telegram bot allows users to search for weather information using the OpenW
 - Get a 5-day weather forecast.
 
 - Get the Air Pullution Index (AQI).
+
+- Access various weather maps including clouds, precipitation, pressure, wind, and temperature maps.
 
 - Inline query support for quick weather updates.
 
@@ -48,23 +50,49 @@ Or, if you created the .env file:
     
     set -a; source .env; set +a; python main.py   
 
-In Telegram, use the bot's username and the name of the city that you want to know the current weather or type forecast followed by the name of the city to get a 5 day weather forecast. For example:
+In Telegram, use the bot's username and the name of the city that you want to know the current weather, type forecast followed by the name of the city to get a 5 day weather forecast or even type map followed by a weather type map and the city name. For example:
 
     @YourBotUsername Fortaleza
     @YourBotUsername forecast Fortaleza
+    @YourBotUsername map clouds Worldwide
+    @YourBotUsername map precipitation Worldwide
+    @YourBotUsername map pressure Worldwide
+    @YourBotUsername map wind Worldwide
+    @YourBotUsername map temperature Worldwide
+
 
 The outputs, respectively, should look something like this:
 
     Current Weather in Fortaleza
     Temp: 27.07°C, Scattered Clouds ☁️
     Air Pollution Index (1-5): 1
-        
+
     5-Day Forecast for Fortaleza
     2023-12-24: 30.14°C, Scattered Clouds ☁️
     2023-12-25: 29.93°C, Scattered Clouds ☁️
     2023-12-26: 29.24°C, Light Rain 🌦️
     2023-12-27: 29.57°C, Light Rain 🌦️
     2023-12-28: 29.79°C, Broken Clouds ☁️
+
+Clouds Map for Worldwide: View Map
+
+![Clouds map](https://raw.githubusercontent.com/jhenrique04/weatherinlinebot/master/images/clouds.jpg)
+
+Precipitation Map for Worldwide: View Map
+
+![Precipitation map](https://raw.githubusercontent.com/jhenrique04/weatherinlinebot/master/images/precipitation.jpg)
+
+Pressure Map for Worldwide: View Map
+
+![Pressure map](https://raw.githubusercontent.com/jhenrique04/weatherinlinebot/master/images/pressure.jpg)
+
+Wind Map for Worldwide: View Map
+
+![Wind map](https://raw.githubusercontent.com/jhenrique04/weatherinlinebot/master/images/wind.jpg)
+
+Temperature Map for Worldwide: View Map
+
+![Temperature map](https://raw.githubusercontent.com/jhenrique04/weatherinlinebot/master/images/temperature.jpg)
 
 ## Contributing
 
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import logging
 import requests
 import sys
 import datetime
+import math
 from uuid import uuid4
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 conditions = {
@@ -30,11 +32,13 @@ conditions = {
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Welcome to the Weather Bot! Type the name of a city to get weather updates.")
+    update.message.reply_text(
+        "Welcome to the Weather Bot! Type the name of a city to get weather updates.")
 
 
 def help(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Type the name of a city in inline mode to get weather information.\nFor example:\n@YourBotName New York\nOr, if you want a 5-day forecast:\n@YourBotName forecast New York")
+    update.message.reply_text(
+        "Type the name of a city in inline mode to get weather information.\nFor example:\n@YourBotName New York\nOr, if you want a 5-day forecast:\n@YourBotName forecast New York")
 
 
 def get_weather(city: str):
@@ -65,6 +69,25 @@ def get_air_pollution(lat: str, lon: str):
     return response.json()
 
 
+def lat_lon_to_tile(lat, lon, zoom):
+    lat_rad = math.radians(lat)
+    n = 2.0 ** zoom
+    x_tile = int((lon + 180.0) / 360.0 * n)
+    y_tile = int((1.0 - math.log(math.tan(lat_rad) + 1 / math.cos(lat_rad)) / math.pi) / 2.0 * n)
+    return x_tile, y_tile
+
+
+def get_weather_map(layer: str, lat: str, lon: str, zoom=1):
+    url = f"https://tile.openweathermap.org/map/{layer}/{zoom}/{lat}/{lon}.png?appid={os.environ.get('OW_API_KEY')}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(url)
+        return url
+    else:
+        logger.error(f"Failed to get weather map: {response.status_code}, {response.text}")
+        return None
+
+
 def create_article(title, message):
     return InlineQueryResultArticle(
         id=uuid4(),
@@ -86,7 +109,8 @@ def format_forecast_message(forecast_data):
             emoji = conditions.get(condition, "")
             temp = day_data['main']['temp']
             description = day_data['weather'][0]['description'].title()
-            message_lines.append(f"{forecast_date.date()}: {temp}°C, {description} {emoji}")
+            message_lines.append(
+                f"{forecast_date.date()}: {temp}°C, {description} {emoji}")
     return "\n".join(message_lines)
 
 
@@ -106,14 +130,116 @@ def inlinequery(update: Update, context: CallbackContext) -> None:
         return
 
     if query.lower().startswith("forecast "):
-        city = query[8:].strip()
+        city = query.split(" ", 1)[1].strip()
         forecast_data = get_forecast(city)
         if forecast_data and 'list' in forecast_data:
             message = format_forecast_message(forecast_data)
-            results.append(create_article(f"5-Day Forecast for {city.title()}", message))
+            results.append(create_article(
+                f"5-Day Forecast for {city.title()}", message))
         else:
-            results.append(create_article("Forecast not found", "Could not retrieve the forecast."))
+            results.append(create_article("Forecast not found",
+                           "Could not retrieve the forecast."))
 
+    if query.lower().startswith("map clouds "):
+        city = query.split(" ", 2)[2].strip()
+        lat, lon = get_coordinates(city)
+        if lat and lon:
+            x_tile, y_tile = lat_lon_to_tile(lat, lon, 1)
+            map_url = get_weather_map("clouds_new", x_tile, y_tile)
+            results.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=f"Clouds Map for {city.title()}",
+                input_message_content=InputTextMessageContent(
+                    message_text=f"Clouds Map for {city.title()}: [View Map]({map_url})",
+                    parse_mode="Markdown"
+                ),
+                thumb_url=map_url,
+                description="Clouds Map"
+            ))
+        else:
+            results.append(create_article("Map not found",
+                           "Could not retrieve the map."))
+            
+    if query.lower().startswith("map precipitation "):
+        city = query.split(" ", 2)[2].strip()
+        lat, lon = get_coordinates(city)
+        if lat and lon:
+            x_tile, y_tile = lat_lon_to_tile(lat, lon, 1)
+            map_url = get_weather_map("precipitation_new", x_tile, y_tile)
+            results.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=f"Precipitation Map for {city.title()}",
+                input_message_content=InputTextMessageContent(
+                    message_text=f"Precipitation Map for {city.title()}: [View Map]({map_url})",
+                    parse_mode="Markdown"
+                ),
+                thumb_url=map_url,
+                description="Precipitation Map"
+            ))
+        else:
+            results.append(create_article("Map not found",
+                           "Could not retrieve the map."))
+            
+    if query.lower().startswith("map pressure "):
+        city = query.split(" ", 2)[2].strip()
+        lat, lon = get_coordinates(city)
+        if lat and lon:
+            x_tile, y_tile = lat_lon_to_tile(lat, lon, 1)
+            map_url = get_weather_map("pressure_new", x_tile, y_tile)
+            results.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=f"Pressure Map for {city.title()}",
+                input_message_content=InputTextMessageContent(
+                    message_text=f"Pressure Map for {city.title()}: [View Map]({map_url})",
+                    parse_mode="Markdown"
+                ),
+                thumb_url=map_url,
+                description="Pressure Map"
+            ))
+        else:
+            results.append(create_article("Map not found",
+                           "Could not retrieve the map."))
+            
+    if query.lower().startswith("map wind "):
+        city = query.split(" ", 2)[2].strip()
+        lat, lon = get_coordinates(city)
+        if lat and lon:
+            x_tile, y_tile = lat_lon_to_tile(lat, lon, 1)
+            map_url = get_weather_map("wind_new", x_tile, y_tile)
+            results.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=f"Wind Map for {city.title()}",
+                input_message_content=InputTextMessageContent(
+                    message_text=f"Wind Map for {city.title()}: [View Map]({map_url})",
+                    parse_mode="Markdown"
+                ),
+                thumb_url=map_url,
+                description="Wind Map"
+            ))
+        else:
+            results.append(create_article("Map not found",
+                           "Could not retrieve the map."))
+            
+    if query.lower().startswith("map temperature "):
+        city = query.split(" ", 2)[2].strip()
+        lat, lon = get_coordinates(city)
+        if lat and lon:
+            x_tile, y_tile = lat_lon_to_tile(lat, lon, 1)
+            map_url = get_weather_map("temp_new", x_tile, y_tile)
+            results.append(InlineQueryResultArticle(
+                id=uuid4(),
+                title=f"Temperature Map for {city.title()}",
+                input_message_content=InputTextMessageContent(
+                    message_text=f"Temperature Map for {city.title()}: [View Map]({map_url})",
+                    parse_mode="Markdown"
+                ),
+                thumb_url=map_url,
+                description="Temperature Map"
+            ))
+        else:
+            results.append(create_article("Map not found",
+                           "Could not retrieve the map."))
+    
     else:
         weather_data = get_weather(query)
         lat, lon = get_coordinates(query)
@@ -135,7 +261,8 @@ def error(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     token = os.environ.get("BOT_TOKEN")
     if not token:
-        logger.critical("No BOT_TOKEN environment variable found. Terminating.")
+        logger.critical(
+            "No BOT_TOKEN environment variable found. Terminating.")
         sys.exit(1)
 
     updater = Updater(token, use_context=True)
